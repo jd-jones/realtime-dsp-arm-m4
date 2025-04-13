19216197c185c30e6bf647280e7db548fa6304d8
Jonathan Jones <jones.jonathan.david@gmail.com>
2025-04-13 14:06:39 -0400

    style fixes for main.c

--- 2025-03_audio-playback/app/main.c
+++ 2025-03_audio-playback/app/main.c
@@ -8,29 +8,30 @@
   ******************************************************************************
   */
 
-#include <stm32f4xx_ll_bus.h>
-#include <stm32f4xx_ll_cortex.h>
-#include <stm32f4xx_ll_gpio.h>
-#include <stm32f4xx_ll_rcc.h>
-#include <stm32f4xx_ll_utils.h>
+#include <stm32f4xx_ll_bus.h>       // LL_AHB1_GRP1_EnableClock
+#include <stm32f4xx_ll_cortex.h>    // LL_SYSTICK_SetClkSource
+#include <stm32f4xx_ll_gpio.h>      // GPIO functions & definitions
+#include <stm32f4xx_ll_utils.h>     // LL_Init1msTick, LL_mDelay
 
 
 #define GREEN_LED_PIN       LL_GPIO_PIN_12
 #define ORANGE_LED_PIN      LL_GPIO_PIN_13
 #define RED_LED_PIN         LL_GPIO_PIN_14
 #define BLUE_LED_PIN        LL_GPIO_PIN_15
-#define LED_PORT    GPIOD
+#define LED_PORT            GPIOD
+#define FOUR_MHz            4000000
 
 
 int main(int argc, char const* argv[]) {
     (void)argc;
     (void)argv;
 
-    // init systick at 4 MHz
-    LL_Init1msTick(4000000);
+    // == INITIALIZE SYSTEM CLOCK TO 4 MHZ ====================================
+    LL_Init1msTick(FOUR_MHz);
     LL_SYSTICK_SetClkSource(LL_SYSTICK_CLKSOURCE_HCLK);
+    // ========================================================================
 
-    // init led
+    // == INITIALIZE LED PINS =================================================
     LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
     LL_GPIO_SetPinMode(LED_PORT, GREEN_LED_PIN, LL_GPIO_MODE_OUTPUT);
     LL_GPIO_SetPinMode(LED_PORT, ORANGE_LED_PIN, LL_GPIO_MODE_OUTPUT);
@@ -41,13 +42,16 @@ int main(int argc, char const* argv[]) {
         GREEN_LED_PIN | ORANGE_LED_PIN | RED_LED_PIN | BLUE_LED_PIN,
         LL_GPIO_OUTPUT_PUSHPULL
     );
+    // ========================================================================
 
     while (1) {
+        // == BLINK LEDS BY TOGGLING EVERY 500 MS =============================
         LL_mDelay(500);
         LL_GPIO_TogglePin(
             LED_PORT,
             GREEN_LED_PIN | ORANGE_LED_PIN | RED_LED_PIN | BLUE_LED_PIN
         );
+        // ====================================================================
     }
 
     return 0;
